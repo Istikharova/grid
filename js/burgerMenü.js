db72bdef3dff9717abb419917422aa6beca8a277
Dana Istikharova <dana.istikharova@bluewin.ch>
2022-08-13 01:28:53 +0200

    first commit

---
+++ js/burgerMenü.js
@@ -0,0 +1,22 @@
+const burger = document.querySelector('.burger');
+const kreuz = document.querySelector('.kreuz');
+const nav = document.querySelector('.nav-list');
+
+function navSlide() {
+
+
+
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+    })
+
+    kreuz.addEventListener('click', () => {
+        nav.classList.remove('nav-active');
+
+    })
+
+}
+
+navSlide();
